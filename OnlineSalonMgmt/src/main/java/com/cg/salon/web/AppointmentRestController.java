5c79db79074f7ad30e3eb1eb28370d9675c1a223
SANATAN K SAHA <SANATAN K SAHA@LAPTOP-43D4NS7C>
2021-05-18 14:50:45 +0530

    appointment done

--- OnlineSalonMgmt/src/main/java/com/cg/salon/web/AppointmentRestController.java
+++ OnlineSalonMgmt/src/main/java/com/cg/salon/web/AppointmentRestController.java
@@ -3,19 +3,30 @@ package com.cg.salon.web;
 import java.util.List;
 
 import javax.management.ServiceNotFoundException;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.salon.dto.AppointmentDto;
 import com.cg.salon.dto.AppointmentSuccessMessage;
+import com.cg.salon.dto.SalonServiceSuccessMessage;
 import com.cg.salon.entity.Appointment;
 import com.cg.salon.exceptions.AppointmentNotFoundException;
 import com.cg.salon.exceptions.CustomerNotFoundException;
+import com.cg.salon.exceptions.SalonServiceNotFoundException;
+import com.cg.salon.exceptions.ValidateAppointmentException;
+import com.cg.salon.exceptions.ValidateSalonServiceException;
 import com.cg.salon.service.IAppointmentService;
 
+@RestController
 public class AppointmentRestController {
 	
 	@Autowired
@@ -23,7 +34,6 @@ public class AppointmentRestController {
 	
 	@GetMapping("viewappointmentbyid/{appId}")
 	public Appointment viewAppointmentById(@PathVariable("appId")int appId) throws AppointmentNotFoundException {
-		System.out.println("get");
 		return viewAppointmentById(appId);
 		
 	}
@@ -40,20 +50,35 @@ public class AppointmentRestController {
 		
 	}
 
-	@PutMapping("addappointmentbycustomerid/{custid}")
-	public Appointment addAppointmentByCustomerId(@PathVariable("custid")int custId) throws CustomerNotFoundException {
-		return addAppointmentByCustomerId(custId);
+	@PostMapping("addappointment")
+	public AppointmentSuccessMessage addAppointment(@Valid @RequestBody AppointmentDto appdto, BindingResult br) throws SalonServiceNotFoundException, ValidateAppointmentException {
+		if (br.hasErrors())
+			throw new ValidateAppointmentException(br.getFieldErrors());
+		int aid= service.addAppointment(appdto);
+		
+		return new AppointmentSuccessMessage("Your generated appointment id is "+ aid);
 		
 	}
-	@PutMapping("addappointmentbyscheduleid/{sid}")
-	public Appointment addAppointmentByScheduleId(@PathVariable("sid")int serviceScheduleId) throws ServiceNotFoundException {
-		return addAppointmentByScheduleId(serviceScheduleId);
-		
+	
+	@PutMapping("editappointment")
+	public AppointmentSuccessMessage editAppointment(@Valid @RequestBody AppointmentDto appdto, BindingResult br) throws SalonServiceNotFoundException, AppointmentNotFoundException, ValidateAppointmentException {
+		if (br.hasErrors())
+		{
+			throw new ValidateAppointmentException(br.getFieldErrors());
+		}
+		service.editAppointment(appdto);
+		return new AppointmentSuccessMessage("Appointment Edited successfully");
 	}
-	@DeleteMapping
-	public boolean deleteAppointmentByAppointmentId(@PathVariable("appointmentid")int appointmentId) throws AppointmentNotFoundException {
-		return deleteAppointmentByAppointmentId(appointmentId);
-		
+	
+	@DeleteMapping("cancelappointment/{appid}")
+	public AppointmentSuccessMessage removeAppointment(@PathVariable ("appid") int appointmentId) throws AppointmentNotFoundException {
+		service.removeAppointment(appointmentId);	
+		return new AppointmentSuccessMessage("Appointment deleted Successfully");
+	}
+	
+	@GetMapping("viewallappointment")
+	public List<Appointment> viewAllAppointments() throws AppointmentNotFoundException {
+		return service.viewAllAppointment();
 	}
 
 }
