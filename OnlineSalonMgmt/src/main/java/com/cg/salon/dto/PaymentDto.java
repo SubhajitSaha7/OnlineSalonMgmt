9d9b1876cb7f8952e22c9b984a72ac8e0f342ea2
SANATAN K SAHA <SANATAN K SAHA@LAPTOP-43D4NS7C>
2021-05-19 10:38:07 +0530

    schedule latest

--- OnlineSalonMgmt/src/main/java/com/cg/salon/dto/PaymentDto.java
+++ OnlineSalonMgmt/src/main/java/com/cg/salon/dto/PaymentDto.java
@@ -15,52 +15,60 @@ import com.cg.salon.entity.BankAccount;
 public class PaymentDto {
 
 	private long paymentId;
-	
+
 	private String type;
-	
+
 	private String status;
-	 
+
 	private Appointment appointment;
-	
+
 	private BankAccount bankAccount;
-	
-	
+
 	public long getPaymentId() {
 		return paymentId;
 	}
+
 	public void setPaymentId(long paymentId) {
 		this.paymentId = paymentId;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public Appointment getAppointment() {
 		return appointment;
 	}
+
 	public void setAppointment(Appointment appointment) {
 		this.appointment = appointment;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
+
 	public BankAccount getBankAccount() {
 		return bankAccount;
 	}
+
 	public void setBankAccount(BankAccount bankAccount) {
 		this.bankAccount = bankAccount;
 	}
-	
+
 	public PaymentDto() {
 		super();
-		
+
 	}
+
 	public PaymentDto(long paymentId, String type, String status, Appointment appointment, BankAccount bankAccount) {
 		super();
 		this.paymentId = paymentId;
@@ -69,6 +77,5 @@ public class PaymentDto {
 		this.appointment = appointment;
 		this.bankAccount = bankAccount;
 	}
-	
 
 }
