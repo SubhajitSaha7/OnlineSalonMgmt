3a4ebca270d78f417ab0670c9879097f73c5265d
SubhajitSaha7 <subhajit1cool@gmail.com>
2021-06-30 13:03:37 +0530

    customer done by subhajit

--- OnlineSalonMgmt/src/main/java/com/cg/salon/web/CustomerAdvice.java
+++ OnlineSalonMgmt/src/main/java/com/cg/salon/web/CustomerAdvice.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.cg.salon.dto.CustomerErrorMessage;
 import com.cg.salon.exceptions.CustomerNotFoundException;
 import com.cg.salon.exceptions.ValidateCustomerException;
 
+@RestControllerAdvice
 public class CustomerAdvice extends GlobalAdvice {
 
 	@ExceptionHandler(CustomerNotFoundException.class)
